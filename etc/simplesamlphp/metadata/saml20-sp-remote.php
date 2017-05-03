1d6447a8010418bb6a9e3103af50963ae1135dba
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2017-05-03 22:39:36 +0200

    two new SP

--- etc/simplesamlphp/metadata/saml20-sp-remote.php
+++ etc/simplesamlphp/metadata/saml20-sp-remote.php
@@ -1,4 +1,48 @@
 <?php
+$metadata['http://jenkins.example.com:8080/jenkins/securityRealm/finishLogin'] = array (
+  'AssertionConsumerService' => 'http://jenkins.example.com:8080/jenkins/securityRealm/finishLogin',
+  'SingleLogoutService' => 'http://jenkins.example.com:8080/jenkins/securityRealm/logOut',
+  'authproc' => array(
+    /* add schacHomeOrganization attribute */
+    10 => array(
+        'class' => 'core:AttributeAdd',
+        'schacHomeOrganization' => 'university.example.org',
+    ),
+    /* add the 'urn' prefix to all supported attributes (if available from authentication source) */
+    90 => array(
+      'class' => 'core:AttributeMap',
+      'uid' => 'urn:mace:dir:attribute-def:uid',
+      'sn' => 'urn:mace:dir:attribute-def:sn',
+      'givenName' => 'urn:mace:dir:attribute-def:givenName',
+      'cn' => 'urn:mace:dir:attribute-def:cn',
+      'displayName' => 'urn:mace:dir:attribute-def:displayName',
+      'mail' => 'urn:mace:dir:attribute-def:mail',
+      'groups' => 'urn:mace:dir:attribute-def:groups',
+    ),
+  ),
+);
+$metadata['http://jenkins.example.com:9090/jenkins/securityRealm/finishLogin'] = array (
+  'AssertionConsumerService' => 'http://jenkins.example.com:9090/jenkins/securityRealm/finishLogin',
+  'SingleLogoutService' => 'http://jenkins.example.com:9090/jenkins/securityRealm/logOut',
+  'authproc' => array(
+    /* add schacHomeOrganization attribute */
+    10 => array(
+        'class' => 'core:AttributeAdd',
+        'schacHomeOrganization' => 'university.example.org',
+    ),
+    /* add the 'urn' prefix to all supported attributes (if available from authentication source) */
+    90 => array(
+      'class' => 'core:AttributeMap',
+      'uid' => 'urn:mace:dir:attribute-def:uid',
+      'sn' => 'urn:mace:dir:attribute-def:sn',
+      'givenName' => 'urn:mace:dir:attribute-def:givenName',
+      'cn' => 'urn:mace:dir:attribute-def:cn',
+      'displayName' => 'urn:mace:dir:attribute-def:displayName',
+      'mail' => 'urn:mace:dir:attribute-def:mail',
+      'groups' => 'urn:mace:dir:attribute-def:groups',
+    ),
+  ),
+);
 $metadata['http://jenkins.example.com:9090/securityRealm/finishLogin'] = array (
   'AssertionConsumerService' => 'http://jenkins.example.com:9090/securityRealm/finishLogin',
   'SingleLogoutService' => 'http://jenkins.example.com:9090/securityRealm/logOut',
